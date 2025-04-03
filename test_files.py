3b79bd4fb39ce1d5aad69b20725225e46cedf01d
nootromaniac <joelkim213@gmail.com>
2025-04-02 23:51:19 -0500

    fixes

--- test_files.py
+++ test_files.py
@@ -32,8 +32,8 @@ def TestPointOne(data):
             "body": fdp.ConsumeString(1000),         
             "sender": "test@example.com"  
     }
-    result = email_detector.EmailDetector.analyze(email_data) 
+    result = email_detector.EmailDetector.analyze(**email_data) 
     assert result["is_threat"] == "legitimate"
-    
+
 atheris.Setup(sys.argv, TestPointOne)
 atheris.Fuzz()
